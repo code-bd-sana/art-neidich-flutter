83c1ddf74040ecb1a038f591df202561a93d077f
flutterdevrabby <fajlarabby220@gmail.com>
2026-01-10 15:18:40 +0600

    inspection progress has done

--- lib/features/inspector_role/inspection_progress/presentation/inspection_progress_screen.dart
+++ lib/features/inspector_role/inspection_progress/presentation/inspection_progress_screen.dart
@@ -1,6 +1,7 @@
 import 'package:artneidich_app/common_widget/custom_button.dart';
 import 'package:artneidich_app/common_widget/custom_text_field.dart';
 import 'package:artneidich_app/gen/assets.gen.dart';
+import 'package:artneidich_app/helpers/navigation_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -261,7 +262,80 @@ class _InspectionProgressScreenState extends State<InspectionProgressScreen> {
             UIHelper.verticalSpace(30.h),
             Padding(
               padding: EdgeInsetsGeometry.symmetric(horizontal: 16.w),
-              child: CustomButton(onPressed: () {}, text: "Submit"),
+              child: CustomButton(
+                onPressed: () async {
+                  await showDialog(
+                    context: context,
+                    builder: (_) {
+                      return AlertDialog(
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          spacing: 8.h,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Inspection successfully completed",
+                              style: TextFontStyle.headLine14c323539InterW400
+                                  .copyWith(
+                                    color: const Color(0xFF09090B),
+                                    fontSize: 16.sp,
+
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                            ),
+
+                            Text(
+                              "Congrats! the assigned 10,001 job successfully completed. ",
+                              style: TextFontStyle.headLine14c323539InterW400
+                                  .copyWith(
+                                    color: const Color(0xFF71717A),
+                                    fontSize: 14.sp,
+
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                            ),
+
+                            Row(
+                              spacing: 16.w,
+                              children: [
+                                Expanded(
+                                  child: CustomButton(
+                                    color: Color(0xFFF4F4F5),
+                                    onPressed: () {
+                                      NavigationService.goBack;
+                                    },
+                                    style: TextFontStyle
+                                        .headLine16c2D8D7CInterW700
+                                        .copyWith(
+                                          color: const Color(0xFF09090B),
+                                          fontSize: 14.sp,
+
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                    text: "Close",
+                                  ),
+                                ),
+
+                                Expanded(
+                                  flex: 2,
+                                  child: CustomButton(
+                                    onPressed: () {
+                                      NavigationService.goBack;
+                                    },
+
+                                    text: "Start Inspection",
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+                text: "Submit",
+              ),
             ),
 
             UIHelper.verticalSpaceSemiLarge,
